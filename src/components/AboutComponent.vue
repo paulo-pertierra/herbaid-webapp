812368ee03710ca45b96c4d43e36a85c995cd585
Paulo Pertierra <paulo.pertierra@gmail.com>
2023-02-16 00:42:49 +0800

    About page redo

--- src/components/AboutComponent.vue
+++ src/components/AboutComponent.vue
@@ -1,4 +1,4 @@
 <template>
-  <v-img src="/devs.jpg" alt=""></v-img>
-  <v-img src="/about.jpg" width="500px" class="mx-auto"></v-img>
+  <v-img src="/devs.jpg" alt="" width="1020px" class="mx-auto"></v-img>
+  <v-img src="/about.jpg" width="1020px" class="mx-auto"></v-img>
 </template>
